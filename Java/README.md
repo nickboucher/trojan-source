3350b24cb85717eb24acc078755c85d65853bc3d
Nicholas Boucher <nicholas.d.boucher@gmail.com>
2021-07-24 17:08:06 -0400

    Add Java

---
+++ Java/README.md
@@ -0,0 +1,9 @@
+# Java
+
+## Reordered Conditional
+
+- Confirmed working on openjdk 16.0.1 2021-04-20 (MacOS via Homebrew)
+
+## Reordered Conditional 2
+
+- Confirmed working on openjdk 16.0.1 2021-04-20 (MacOS via Homebrew)
---
+++ Java/ReorderedConditional2.java
@@ -0,0 +1,10 @@
+public class ReorderedConditional2 {
+
+    public static void main(String[] args) {
+        String accessLevel = "user";
+        if (accessLevel != "user‮ ⁦// Check if admin⁩ ⁦") {
+            System.out.println("You are an admin.");
+        }
+    }
+
+}
