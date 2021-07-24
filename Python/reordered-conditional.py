88917cf6dc0f66f199a228910b4da50f3043e961
Nicholas Boucher <nicholas.d.boucher@gmail.com>
2021-07-24 17:05:50 -0400

    Simplify Python

--- Python/reordered-conditional.py
+++ Python/reordered-conditional.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python3
 
-def main():
-    access_level = "user"
-    if access_level != 'none‮⁦': # Check if admin ⁩⁦' and access_level != 'user⁩
-        print("You are an admin.")
-
-
-if __name__ == '__main__':
-    main()
+access_level = "user"
+if access_level != 'none‮⁦': # Check if admin ⁩⁦' and access_level != 'user
+    print("You are an admin.")
