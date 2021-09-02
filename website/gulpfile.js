c9323ca5f4707dc6b6c1be2cc40e251bdd1fc4be
Nicholas Boucher <nicholas.d.boucher@gmail.com>
2021-09-01 23:25:09 -0400

    Remove comments

--- website/gulpfile.js
+++ website/gulpfile.js
@@ -2,17 +2,14 @@ const gulp = require("gulp");
 const gap = require("gulp-append-prepend");
 
 gulp.task("licenses", async function () {
-  // this is to add Creative Tim licenses in the production mode for the minified js
   gulp
     .src("build/static/js/*chunk.js", { base: "./" })
     .pipe(gulp.dest("./", { overwrite: true }));
 
-  // this is to add Creative Tim licenses in the production mode for the minified html
   gulp
     .src("build/index.html", { base: "./" })
     .pipe(gulp.dest("./", { overwrite: true }));
 
-  // this is to add Creative Tim licenses in the production mode for the minified css
   gulp
     .src("build/static/css/*chunk.css", { base: "./" })
     .pipe(gulp.dest("./", { overwrite: true }));
