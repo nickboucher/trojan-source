f93ceee6d2c5fd4ba0bf92965b69ee1328df1cca
Jamie Finnigan <jfinnigan@hashicorp.com>
2021-11-01 17:23:07 -0700

    test

--- Go/commenting-out.go
+++ Go/commenting-out.go
@@ -7,7 +7,7 @@ func main() {
     var isAdmin = false
     var isSuperAdmin = false
     isAdmin = isAdmin || isSuperAdmin
-    /*‮ } ⁦if (isAdmin)⁩ ⁦ begin admins only */
+    /*‮ } ⁦if (isAdmin)⁩ yep begin admins only */
         fmt.Println("You are an admin.")
     /* end admins only ‮ { ⁦*/
 }
