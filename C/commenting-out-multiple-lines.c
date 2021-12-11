1363b1f92a9a870438daf4749afa9a0733617bef
Abdun Nihaal <abdun.nihaal@gmail.com>
2021-12-11 19:09:52 +0530

    Add POC for commenting out multiple lines in C

---
+++ C/commenting-out-multiple-lines.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main() {
+	printf("Hi\n");
+	/* Hello ‮ ⁦printf("Hello World\n");⁩ ⁦*​/⁩
+	printf("Hello\n");
+	printf("Hello again\n");
+	/*  Bye  ‮ ⁦printf("Bye World\n");⁩ ⁦*/
+ 	return 0;
+ }
